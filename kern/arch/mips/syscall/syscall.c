e2447a7adbf32f22cd1a7615d7f4ff74bc96b976
Aditya Bhattacharya <arb17b@my.fsu.edu>
2017-11-28 18:17:59 -0500

    Update syscall.c

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -134,10 +134,7 @@ syscall(struct trapframe *tf)
                 break;
 	  case SYS_close:
                 err = sys_close(
-                        tf->tf_a0,
-                        (userptr_t)tf->tf_a1,
-                        tf->tf_a2,
-                        &retval);
+                        tf->tf_a0);
                 break;
 
             /* Project 3:
