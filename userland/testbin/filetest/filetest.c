cfdc34a908a31171f2a5e95c5b753e4edff4a4d0
Aditya Bhattacharya <arb17b@my.fsu.edu>
2017-11-29 17:52:15 -0500

    Update filetest.c

--- userland/testbin/filetest/filetest.c
+++ userland/testbin/filetest/filetest.c
@@ -64,12 +64,47 @@ main(int argc, char *argv[])
 	}
 
 	fd = open(file, O_WRONLY|O_CREAT|O_TRUNC, 0664);
+	if (fd<0) {
+		err(1, "%s: open for write", file);
+	}
+
+
 	rv = write(fd, writebuf, 40);
+	if (rv<0) {
+		err(1, "%s: write", file);
+	}
+
 	rv = close(fd);
-	
+	if (rv<0) {
+		err(1, "%s: close (1st time)", file);
+	}
+
 	fd = open(file, O_RDONLY);
+	if (fd<0) {
+		err(1, "%s: open for read", file);
+	}
+
 	rv = read(fd, readbuf, 40);
+	if (rv<0) {
+		err(1, "%s: read", file);
+	}
+
 	rv = close(fd);
-	(void)rv;
+	printf("Closing fd=%d retval=%d.\n", fd, rv);
+	if (rv<0) {
+		err(1, "%s: close (2nd time)", file);
+	}
+	/* ensure null termination */
+	readbuf[40] = 0;
+
+	if (strcmp(readbuf, writebuf)) {
+		errx(1, "Buffer data mismatch!");
+	}
+
+/*	rv = remove(file);
+	if (rv<0) {
+		err(1, "%s: remove", file);
+	} */
+	printf("Passed filetest.\n");
 	return 0;
 }
