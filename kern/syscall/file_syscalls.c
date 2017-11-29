37b66a7b4dd57f30da682b5856820c9bbed5079a
Aditya Bhattacharya <arb17b@my.fsu.edu>
2017-11-29 12:35:42 -0500

    Update file_syscalls.c

--- kern/syscall/file_syscalls.c
+++ kern/syscall/file_syscalls.c
@@ -107,7 +107,7 @@ sys_read(int fd, userptr_t buf, size_t size, int *retval)
 	uio_kinit(&io, &reader, &buf, size, file->of_offset, UIO_READ);
 	reader.uio_segflg = UIO_USERSPACE;
 	reader.uio_space = curproc->p_addrspace;
-	result = VOP_READ(file->of_vnode, reader);
+	result = VOP_READ(file->of_vnode, &reader);
 	*retval = size - reader.uio_resid;
 	lock_release(file->of_offsetlock);
 	filetable_put(curproc->p_filetable,fd, file);
