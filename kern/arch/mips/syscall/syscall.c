6b29ddaf0d14373fc906b26da8af2b0da128f64b
Aditya Bhattacharya <arb17b@my.fsu.edu>
2017-12-01 11:34:38 -0500

    Update syscall.c

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -140,7 +140,7 @@ syscall(struct trapframe *tf)
                         tf->tf_a2,
                         &retval);
                 break;
-	  case 57:
+	  case SYS_meld:
                 err = sys_meld(
                         (userptr_t)tf->tf_a0,
                         (userptr_t)tf->tf_a1,
